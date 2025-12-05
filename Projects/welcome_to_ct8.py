c06c6bbeea9e313a900f87d9a0c027ad04dac132
SophieTang1 <Sophietang@seattleacademy.org>
2025-12-05 19:51:30 +0000

    test

--- Projects/welcome_to_ct8.py
+++ Projects/welcome_to_ct8.py
@@ -27,4 +27,6 @@ print("\n\n")
                                                                                           
                                   
 print("Now it's your turn:")
-print("Find this line (line 30) in the welcome_to_ct8.py file, then change the message to say a fact about you.")                                  
+print("I am good at math ")
+print("I love summer")
+print("I was born in the US")
